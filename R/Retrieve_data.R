c13c1cdd1a5d9b67e1f10b7205b2975cc598a402
Cherine Jantzen <cherine.jantzen@gmail.com>
2023-12-18 17:06:45 +0100

    Change driver and integrate use of package "here"

--- R/Retrieve_data.R
+++ R/Retrieve_data.R
@@ -2,38 +2,40 @@
 
 # Author: Cherine Jantzen
 # Created: 12/12/2023
+# Last updated: 18/12/2023
 
 # load packages
 library(DBI)
+library(here)
 library(odbc)
 library(rstudioapi)
 library(tidyverse)
 
 # connect to NIOO AnE SQL server to retrieve data
 con <- DBI::dbConnect(drv = odbc::odbc(),
-                      Driver = {'SQL Server'},
-                      UID = rstudioapi::askForPassword('Uid'),
-                      Pwd = rstudioapi::askForPassword('Pwd'),
-                      Server = 'ws.ane.nioo.int')
+                      Driver = {"ODBC Driver 17 for SQL Server"},
+                      UID = rstudioapi::askForPassword("Uid"),
+                      Pwd = rstudioapi::askForPassword("Pwd"),
+                      Server = rstudioapi::askForPassword("Server"))
 
 # get input data
 
 ## tbl_Area
 dplyr::tbl(con, dbplyr::in_catalog(catalog = "AnE_Budburst", schema = "dbo", table = "tbl_Area")) %>%
   dplyr::collect() %>%
-  write.csv('data/tbl_Area.csv')
+  write.csv(here::here("data", "tbl_area.csv"), row.names = FALSE)
 
 ## tbl_Budburst
 dplyr::tbl(con, dbplyr::in_catalog(catalog = "AnE_Budburst", schema = "dbo", table = "tbl_Budburst")) %>%
   dplyr::collect() %>%
-  write.csv('data/tbl_budburst.csv')
+  write.csv(here::here("data", "tbl_budburst.csv"), row.names = FALSE)
 
 ## tbl_Tree
 dplyr::tbl(con, dbplyr::in_catalog(catalog = "AnE_Budburst", schema = "dbo", table = "tbl_Tree")) %>%
   dplyr::collect() %>%
-  write.csv('data/tbl_Tree.csv')
+  write.csv(here::here("data", "tbl_Tree.csv"), row.names = FALSE)
 
 ## tbl_TreeSpecies
 dplyr::tbl(con, dbplyr::in_catalog(catalog = "AnE_Budburst", schema = "dbo", table = "tbl_TreeSpecies")) %>%
   dplyr::collect() %>%
-  write.csv('data/tbl_TreeSpecies.csv')
+  write.csv(here::here("data", "tbl_TreeSpecies.csv"), row.names = FALSE)
