debf3920bf484cfdb1f763bc9a123c2424334dab
Cherine Jantzen <cherine.jantzen@gmail.com>
2023-12-20 13:42:13 +0100

    Update plots for second species

--- R/visuals_climwin_results.R
+++ R/visuals_climwin_results.R
@@ -2,6 +2,7 @@
 
 # Author: Cherine Jantzen
 # Created: 19/12/2023
+# Last updated: 20/12/2023
 
 
 # load packages
@@ -11,11 +12,17 @@ library(ggplot2)
 
 # I. Plots integrated in climwin ------------------------------------------
 
-# First window analysis: plot AIC heat map
-climwin::plotdelta(first_window[[1]]$Dataset)
+# First window analysis Quercus robur: plot AIC heat map
+climwin::plotdelta(first_window_Qrobur[[1]]$Dataset)
+climwin::plotall(first_window_Qrobur[[1]]$Dataset)
 
-# Second window analysis: plot AIC heat map
-climwin::plotall(second_window[[1]]$Dataset)
+# Second window analysis Quercus robur: plot AIC heat map
+climwin::plotdelta(second_window_Qrobur[[1]]$Dataset)
+climwin::plotall(second_window_Qrobur[[1]]$Dataset)
+
+# First window analysis Quercus rubra: plot AIC heat map
+climwin::plotdelta(first_window_Qrubra[[1]]$Dataset)
+climwin::plotall(first_window_Qrubra[[1]]$Dataset)
 
 
 # II. Compare model output with observed values --------------------------
@@ -28,23 +35,23 @@ mean_temp_in_window <- temp %>%
                    .by = c("year", "locID"))
 
 # get the model coefficients
-wind_coef <- coef(summary(first_window[[1]]$BestModel)) 
+wind_coef_Qrobur <- coef(summary(first_window_Qrobur[[1]]$BestModel)) 
 
 # calculate predicted bud burst dates based on model coefficients
-mean_temp_in_window$pred.bb_date <- (wind_coef[1,1] + wind_coef[2,1] * mean_temp_in_window$year) + (wind_coef[3,1] * mean_temp_in_window$mean_temp)
+mean_temp_in_window$pred.bb_date <- (wind_coef_Qrobur[1,1] + wind_coef_Qrobur[2,1] * mean_temp_in_window$year) + (wind_coef_Qrobur[3,1] * mean_temp_in_window$mean_temp)
 
 # calculate mean annual bud burst date 
-obs_pred_bb <- db_2 %>% 
-  summarise(mean_annual_bb_date = mean(avg_budburst_DOY), 
+obs_pred_bb_Qrobur <- budburst_Qrobur %>% 
+  summarise(mean_annual_bb_date = mean(avg_bud_burst_DOY), 
             .by = c("year", "locID")) %>% 
   right_join(mean_temp_in_window, by = c("year", "locID"))
 
 # plot predicted against observed annual bud burst dates 
-ggplot2::ggplot(obs_pred_bb, aes(mean_annual_bb_date, pred.bb_date,  colour = locID)) +
+ggplot2::ggplot(obs_pred_bb_Qrobur, aes(mean_annual_bb_date, pred.bb_date,  colour = locID)) +
   ggplot2::geom_point(size = 2) +
   ggplot2::geom_smooth(aes(colour = locID), method = "lm", se = FALSE) +
   ggplot2::theme_classic() +
-  ggplot2::labs(title = "Predicted & observed bud burst dates", 
+  ggplot2::labs(title = "Predicted & observed bud burst dates of Q. robur", 
                 subtitle = "Climate window: 04/04 to 08/05",
                 y = "predicted bud burst date [day of the year]",
                 x = "observed annual bud burst date [day of the year]") +
