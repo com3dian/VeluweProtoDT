4f5acdccb0e7fd0fa3eac5b944b90ba447dcaf6d
Thijs van der Plas <vdplasthijs@gmail.com>
2025-04-14 18:01:23 +0200

    Remove old deterministic GDD calculation

--- Py/src/probabilistic_utils.py
+++ Py/src/probabilistic_utils.py
@@ -71,38 +71,6 @@ def prep_data_for_regression(budburst_df=None, temp_df=None,
         regression_df['bb_cdf'] = regression_df['bb_cdf'].ffill().fillna(0)
         regression_df['date'] = pd.to_datetime(regression_df['date'], format='%Y-%m-%d')
 
-        if False:
-            ## Calculate GDD
-            t_above_base = np.maximum(0, regression_df['temperature'] - t_base)
-            regression_df['t_above_base'] = t_above_base
-            if gdd_month_day_start is not None:
-                assert type(gdd_month_day_start) == tuple, f"gdd_month_day_start {gdd_month_day_start} is not a tuple"
-                assert len(gdd_month_day_start) == 2, f"gdd_month_day_start {gdd_month_day_start} is not a tuple of length 2"
-                gdd_month, gdd_day = gdd_month_day_start
-                assert type(gdd_month) == int and type(gdd_day) == int, f"gdd_month_day_start {gdd_month_day_start} is not a tuple of integers"
-                assert gdd_month >= 1 and gdd_month <= 12, f"gdd_month {gdd_month} is not between 1 and 12"
-                assert gdd_day >= 1 and gdd_day <= 31, f"gdd_day {gdd_day} is not between 1 and 31"
-                if gdd_month > 9:
-                    gdd_start = datetime(year=y - 1, month=gdd_month, day=gdd_day, tzinfo=regression_df['date'][0].tzinfo)
-                else:
-                    gdd_start = datetime(year=y, month=gdd_month, day=gdd_day, tzinfo=regression_df['date'][0].tzinfo)
-            else:
-                gdd_doy_start = season_start_doy
-                gdd_start = datetime(year=y - 1, month=1, day=1, tzinfo=regression_df['date'][0].tzinfo) + pd.offsets.DateOffset(days=gdd_doy_start - 1)
-            gdd_start = pd.to_datetime(gdd_start, format='%Y-%m-%d')
-            gdd_start = max(gdd_start, regression_df['date'].min())
-            
-            ## gdd is cumulative t_above_base from gdd_start to date
-            regression_df['gdd'] = regression_df['t_above_base'].cumsum()
-            gdd_on_start_day = regression_df[regression_df['date'] == gdd_start]['gdd']
-            assert len(gdd_on_start_day) == 1, f"gdd_on_start_day {gdd_on_start_day} not unique"
-            gdd_on_start_day = gdd_on_start_day.values[0]
-            regression_df['gdd'] = regression_df['gdd'] - gdd_on_start_day
-            if regression_df['gdd'].dtype == pt.tensor.TensorVariable:
-                regression_df['gdd'] = pm.math.maximum(0, regression_df['gdd'])
-            else:
-                regression_df['gdd'] = np.maximum(0, regression_df['gdd'].values)
-            del regression_df['t_above_base']
         s = f'{y-1}-{y}'
         regression_df['season'] = s
 
