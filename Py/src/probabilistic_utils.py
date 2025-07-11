Fix sigma scaling

Py/src/probabilistic_utils.py
@@ -175,8 +175,9 @@ def bayesian_inference(
         ## Likelihood: Normal distribution with uncertainty
         sigma = pm.HalfNormal("sigma", sigma=0.1)
         if scale_sigma_by_mu:
-            ## set sigma = abs(mu - 0.5) * sigma 
-            std = pm.Deterministic("sigma_scaled", pt.abs(mu - 0.5) * sigma)
+            ## sigma * (1 - 2 * np.abs(mu - 0.5)) + 1e-3
+            eps = 1e-3
+            std = pm.Deterministic("sigma_scaled", (1 - 2 * pt.abs(mu - 0.5)) * sigma + eps)  # Avoid division by zero
         else:
             std = sigma
         bb_cdf_likelihood = pm.Normal("bb_cdf", mu=mu, sigma=std, 